a2717c8382fdc46040dfe0aec5c2b5a88b01c0d7
Lauren Santosuosso <Lauren.Santosuosso@yahoo.com>
2019-07-16 22:42:22 -0400

    trying to get this bird to fly

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+// DEPENDENCIES
 var express = require("express");
+
+
+// EXPRESS CONFIGURATION
+
 var app = express();
+var PORT = process.env.PORT || 8080;
 
+// express.json and express.urlEncoded make it easy for our server to interpret data sent to it.
+// The code below is pretty standard.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
+// ROUTER
+
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+// require("./Spider/assets/index.html")(app);
+// ==============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// ==============================================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
 });
